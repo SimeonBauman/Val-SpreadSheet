36c8e6bfcb1417205a686559b14329376869db14
SimeonBauman <96099765+SimeonBauman@users.noreply.github.com>
2024-07-01 21:41:31 -0500

    Created Project

---
+++ ValorantDataScraper/ValorantDataScraper.py
@@ -0,0 +1,29 @@
+from selenium import webdriver   
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup 
+import time
+  
+
+
+
+service = Service(executable_path="C:\chromedriver.exe")
+options = webdriver.ChromeOptions()
+options.add_argument("user-data-dir=C:/Users/simeo/AppData/Local/Google/Chrome/User Data")
+#options.add_argument("--incognito")
+driver = webdriver.Chrome(service = service,options=options)
+   
+# Navigate to the website   
+base_url = "https://tracker.gg/valorant"
+
+driver.get(base_url)  
+
+while(True):
+    html_source = driver.page_source   
+    soup = BeautifulSoup(html_source, 'html.parser') 
+    print(soup.findAll(class_="trn-ign__username"))
+    time.sleep(5)
+
+   
+# Close the browser to free up resources  
+driver.close()
